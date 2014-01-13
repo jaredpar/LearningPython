25406988965017ecc89552be1f16b0461ce49d04
Jared Parsons <jaredpar@microsoft.com>
2014-01-13 09:16:52 -0800

    Add answers today to the stats

--- get-answers.py
+++ get-answers.py
@@ -4,6 +4,7 @@ import gzip
 import time
 import calendar
 from datetime import date
+from datetime import timedelta
 from StringIO import StringIO
 
 #tempUrl = 'http://api.stackexchange.com/2.1/users/23283/answers?fromdate=1389312000&order=desc&sort=activity&site=stackoverflow'
@@ -14,18 +15,23 @@ class Answer(object):
     def __init__(self, json):
         self.accepted = json['is_accepted']
         self.score = json['score']
+        self.createdSeconds = json['creation_date']
         self.created = time.gmtime(json['creation_date'])
         self.id = json['answer_id']
         self.quesionId = json['question_id']
+
+# Convert a date value into seconds since the epoch in UTC
+def dateToSecondsUtc(d):
+    str = '{0} {1} {2}'.format(d.year, d.month, d.day) 
+    t = time.strptime(str, '%Y %m %d')
+    t = int(calendar.timegm(t))
+    return t
 
 # Return a tuple of start time, end time for the query 
 def getTimeRange():
     startTime = time.strptime('2014 1 1', '%Y %m %d')
     startTime = int(calendar.timegm(startTime))
-    endDay = date.today()
-    endDayStr = '{0} {1} {2}'.format(endDay.year, endDay.month, endDay.day) 
-    endTime = time.strptime(endDayStr, '%Y %m %d')
-    endTime = int(calendar.timegm(endTime))
+    endTime = dateToSecondsUtc(date.today() + timedelta(days=1))
     return (startTime, endTime)
 
 # Build up the start and end date for the query 
@@ -35,17 +41,24 @@ def getApiUrl(startTime, endTime, page):
     apiUrlFormat = 'http://api.stackexchange.com/2.1/users/23283/answers?page={0}&fromdate={1}&todate={2}&order=desc&sort=activity&site=stackoverflow'
     return apiUrlFormat.format(page, startTime, endTime)
 
-def printStats(answerList):
-    total = len(answerList)
+def printStats(startTime, endtime, answerList):
+    totalYear = len(answerList)
+
+    # To get the answers today just find any with a time stamp greater than
+    # midnight today
+    todayStartSeconds = dateToSecondsUtc(date.today())
+    totalDay = sum(x.createdSeconds >= todayStartSeconds for x in answerList)
+
     diff = date.today() - date(2014, 1, 1)
     days = diff.days
     weeks = days / 7.0
-    averageDay = float(total) / days
-    averageWeek = float(total) / weeks
+    averageDay = float(totalYear) / days
+    averageWeek = float(totalYear) / weeks
     print '---'
-    print 'Total: {0}'.format(total)
-    print 'Average per day: {0}'.format(averageDay)
-    print 'Average per week: {0}'.format(averageWeek)
+    print 'Total Year: {0}'.format(totalYear)
+    print 'Total Today: {0}'.format(totalDay)
+    print 'Average Per Day: {0}'.format(averageDay)
+    print 'Average Per Week: {0}'.format(averageWeek)
 
 def getAnswerList(startTime, endTime):
     print 'Range {0} - {1}'.format(startTime, endTime)
@@ -78,4 +91,4 @@ def getAnswerList(startTime, endTime):
 
 dateRange = getTimeRange()
 answerList = getAnswerList(dateRange[0], dateRange[1])
-printStats(answerList)
+printStats(dateRange[0], dateRange[1], answerList)
