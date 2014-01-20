ca4652d2619d52b76d2206b53676cb2852c205cf
Jared Parsons <jaredpparsons@gmail.com>
2014-01-20 14:41:37 -0800

    awful version of show command
    This will display the contents of an object in the index.  But only if
    it doesn't appear in a pack file.  Really trivial stuff here :)

--- ag.py
+++ ag.py
@@ -1,4 +1,25 @@
+import zlib
+import os
 
 def show(sha1):
-    print sha1
+    prefix = sha1[0:2]
+    suffix = sha1[2:]
+
+    # First find the file path 
+    dirPath = os.path.join(r'.git\objects', prefix)
+    files = os.listdir(dirPath)
+    files = filter(lambda f: f.startswith(suffix), files)
+    if len(files) == 0:
+        print 'Could not find {0}'.format(sha1)
+        return
+    if len(files) > 1:
+        print 'Multiple possible matches for {0}'.format(sha1)
+        return
+
+    filePath = os.path.join(dirPath, files[0])
+    with open(filePath) as f:
+        bytes = f.read()
+        data = zlib.decompress(bytes)
+
+    print data
 
