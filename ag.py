48590a2ec8fbe05d5ba811d86abf5782d67ae6c3
Jared Parsons <jaredpar@microsoft.com>
2014-01-20 16:46:29 -0800

    Can print out the pack entries now

--- ag.py
+++ ag.py
@@ -1,5 +1,7 @@
 import zlib
 import os
+import struct
+import sys
 
 def show(sha1):
     prefix = sha1[0:2]
@@ -28,4 +30,34 @@ def show(sha1):
         data = data[index + 1:]
 
     print data
+
+def printPack():
+    # The test pack we are working with for now 
+    packIndexPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.idx'
+
+    # just assuming version 2 of the .idx format for now
+    # https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt
+
+    with open(packIndexPath, 'rb') as f:
+
+        # Magic number 
+        f.read(4) 
+
+        # Version number
+        f.read(4)
+
+        total = 0
+        for x in range(0, 256):
+            bytes = f.read(4)
+            number = struct.unpack('>i', bytes)[0]
+            print "{0} - {1}".format(x, number)
+            total = number
+
+        # Print out all of the SHA1 values stored in the pack file 
+        for x in range(0, total):
+            name = f.read(20)
+            for b in name:
+                sys.stdout.write(b.encode('hex'))
+            print ""
+
 
