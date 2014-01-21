4ab7f2e173f4740e2f753b8427f870b727ffce0a
Jared Parsons <jaredpar@microsoft.com>
2014-01-20 21:16:15 -0800

    Can read the pack file index now

--- ag.py
+++ ag.py
@@ -2,6 +2,10 @@ import zlib
 import os
 import struct
 import sys
+from collections import namedtuple
+
+class Expando():
+    pass
 
 def show(sha1):
     prefix = sha1[0:2]
@@ -33,6 +37,25 @@ def show(sha1):
 
 def printPack():
     # The test pack we are working with for now 
+    packPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.pack'
+
+    with open(packPath, 'rb') as f:
+
+        # Magic number 
+        f.read(4) 
+
+        # Version number
+        f.read(4)
+
+        bytes = f.read(4)
+        number = struct.unpack('>i', bytes)[0]
+        print 'Number of items {0}'.format(number)
+
+        bytes = f.read(100)
+        print bytes.encode('hex')
+
+def printPackIndex():
+    # The test pack index we are working with for now 
     packIndexPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.idx'
 
     # just assuming version 2 of the .idx format for now
@@ -50,14 +73,51 @@ def printPack():
         for x in range(0, 256):
             bytes = f.read(4)
             number = struct.unpack('>i', bytes)[0]
+
             print "{0} - {1}".format(x, number)
             total = number
 
-        # Print out all of the SHA1 values stored in the pack file 
+        # Read in all of the SHA1 values 
+        entryList = []
         for x in range(0, total):
             name = f.read(20)
+            sha1 = ''
             for b in name:
-                sys.stdout.write(b.encode('hex'))
-            print ""
+                sha1 += b.encode('hex')
+            entry = Expando()
+            entry.sha1 = sha1
+            entry.offset = ''
+            entry.crc32 = ''
+            entryList.append(entry)
 
+        # Read in all of the CRC32 values
+        for x in range(0, total): 
+            bytes = f.read(4)
+            number = struct.unpack('>i', bytes)[0]
+            entryList[x].crc32 = number
 
+        # Read in all of the offsets
+        for x in range(0, total):
+            bytes = f.read(4)
+            number = struct.unpack('>i', bytes)[0]
+            entryList[x].offset = number
+
+        # TODO: Need to read the 8 byte offset table if any of the high bits were
+        # set in the offset table
+
+        # Closing SHA1 signature pairs
+        f.read(40)
+
+        entryList.sort(key=lambda x: x.offset)
+        for entry in entryList:
+            print '{0} - {1} - {2}'.format(entry.sha1, entry.offset, entry.crc32)
+
+        remaining = f.read()
+        if remaining == '':
+            print 'Hit the end of the file'
+        else:
+            print 'Didn\'t hit the end of the file'
+            print '{0} bytes remaining'.format(len(remaining))
+            print remaining
+
+            
