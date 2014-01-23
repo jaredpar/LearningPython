5370c5b0401f51275fb42deb9ad28ef6b0b12862
Jared Parsons <jaredpar@microsoft.com>
2014-01-23 08:25:31 -0800

    Putting it together
    Can now print out random entries from the pack file

--- ag.py
+++ ag.py
@@ -4,6 +4,10 @@ import struct
 import sys
 from collections import namedtuple
 
+# The test pack we are working with for now 
+testPackPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.pack'
+testPackIndexPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.idx'
+
 class Expando():
     pass
 
@@ -35,9 +39,31 @@ def show(sha1):
 
     print data
 
+def printPackEntry(f):
+    c = struct.unpack('b', f.read(1))[0]
+    type = (c >> 4) & 7
+    size = c & 15
+    shift = 4
+    while (c & 0x80) != 0:
+        if shift >= 32:
+            print 'Bad shift value'
+            return
+
+        c = struct.unpack('b', f.read(1))[0]
+        increment = (c & 0x7f) << shift
+        size += increment
+        shift += 7
+        print 'size {0}'.format(size)
+    print 'type {0} size {1}'.format(type, size)
+
+    bytes = f.read(size)
+    if type == 1 or type == 2 or type == 3: 
+        data = zlib.decompress(bytes)
+        print data
+    else:
+        print 'not analyzing data'
+
 def printPack():
-    # The test pack we are working with for now 
-    packPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.pack'
 
     def parsePackEntry(f):
         c = struct.unpack('b', f.read(1))[0]
@@ -63,7 +89,7 @@ def printPack():
         else:
             print 'not analyzing data'
 
-    with open(packPath, 'rb') as f:
+    with open(testPackPath, 'rb') as f:
 
         # Magic number 
         f.read(4) 
@@ -78,14 +104,12 @@ def printPack():
         for i in range(0, 4):
             parsePackEntry(f)
 
-def printPackIndex():
-    # The test pack index we are working with for now 
-    packIndexPath = r'C:\Users\jaredpar\Documents\GitHub\VsVim\.git\objects\pack\pack-1e4dcc41b28289effa674569532d4074f2c226ef.idx'
+def parsePackIndex():
 
     # just assuming version 2 of the .idx format for now
     # https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt
 
-    with open(packIndexPath, 'rb') as f:
+    with open(testPackIndexPath, 'rb') as f:
 
         # Magic number 
         f.read(4) 
@@ -98,7 +122,7 @@ def printPackIndex():
             bytes = f.read(4)
             number = struct.unpack('>i', bytes)[0]
 
-            print "{0} - {1}".format(x, number)
+            # print "{0} - {1}".format(x, number)
             total = number
 
         # Read in all of the SHA1 values 
@@ -132,16 +156,29 @@ def printPackIndex():
         # Closing SHA1 signature pairs
         f.read(40)
 
-        entryList.sort(key=lambda x: x.offset)
-        for entry in entryList:
-            print '{0} - {1} - {2}'.format(entry.sha1, entry.offset, entry.crc32)
+        return entryList
 
-        remaining = f.read()
-        if remaining == '':
-            print 'Hit the end of the file'
-        else:
-            print 'Didn\'t hit the end of the file'
-            print '{0} bytes remaining'.format(len(remaining))
-            print remaining
+def printPackIndex():
+    entryList = parsePackIndex()
+    entryList.sort(key=lambda x: x.offset)
+    for entry in entryList:
+        print '{0} - {1} - {2}'.format(entry.sha1, entry.offset, entry.crc32)
 
+    remaining = f.read()
+    if remaining == '':
+        print 'Hit the end of the file'
+    else:
+        print 'Didn\'t hit the end of the file'
+        print '{0} bytes remaining'.format(len(remaining))
+        print remaining
+
+def testIt():
+    with open(testPackPath, 'rb') as f:
+        entryList = parsePackIndex()
+        for i in range(0, 5): 
+            entry = entryList[i]
+            print entry.sha1
+
+            f.seek(entry.offset)
+            printPackEntry(f)
             
